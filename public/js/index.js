bb507f6120963e1885d7dda701e43da66b180692
unknown <chugm1989@gmail.com>
2020-07-04 22:07:37 -0400

    functionality added: evaluateBtn, subBtn

--- public/js/index.js
+++ public/js/index.js
@@ -2,26 +2,55 @@
 
 const leftbar = $(".b");
 
+// Arrays to hold button subtopics
+const carearTopics = ["Programming"];
+const fitnessTopics = ["Climbing", "Hiking", "Lifting", "Shufflng"];
+const lifestyleTopics = ["Car Maintenance", "Cooking", "Firearms"];
+
 
 /* Navbar Button DOM Manipulation */
 
-//
+// Navlink button actions
 function displayCarear() {
     leftbar.empty();
-    leftbar.append("<a> Programming </a>");
-};
-
-function displayLifestyle() {
-    leftbar.empty();
-    leftbar.append("<a> Firearms </a>");
+    for (i = 0; i < carearTopics.length; i++) {
+        let btn = $("<button>");
+        btn.addClass("button");
+        btn.attr("button-click");
+        btn.text(carearTopics[i]);
+        leftbar.append(btn);
+    };
 };
 
 function displayFitness() {
     leftbar.empty();
-    leftbar.append("<a> Climbing </a>");
+    for (i = 0; i < fitnessTopics.length; i++) {
+        let btn = $("<button>");
+        btn.addClass("button");
+        btn.attr("button-click");
+        btn.text(fitnessTopics[i]);
+        leftbar.append(btn);
+    };
 };
 
-// Switch statement for button actions
+function displayLifestyle() {
+    leftbar.empty();
+// Display clickable buttons for Lifestyle subtopics
+    for (i = 0; i < lifestyleTopics.length; i++) {
+        let btn = $("<button>");
+        btn.addClass("button subBtn");
+        btn.attr("button-click");
+        btn.text(lifestyleTopics[i]);
+        leftbar.append(btn);
+    }
+};
+
+
+// Navbar subtopic button constructor
+
+
+
+// Switch statement for leftbar display
 function evaluateBtn() {
     let clickedBtn = $(event.target).attr("name");
     
@@ -38,8 +67,15 @@ function evaluateBtn() {
     };
 };
 
-
+// subBtn 
+function subBtn() {
+    console.log("This works");
+};
 
 /* DOM Events */
 
-$("button").on("click", evaluateBtn);
+// Navbar buttons
+$(".navlink").on("click", evaluateBtn);
+
+// Leftbar buttons
+$(leftbar).on("click", subBtn);
